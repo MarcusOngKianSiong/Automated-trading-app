98a58f1067d98f9058308e57af697363efc94b39
Marcus <marcusongkiansiong@gmail.com>
2023-03-30 03:49:29 +0800

    Donut chart responsive to counter in backend and moves accordingly based on counter

--- src/pages/execute/donutChart.jsx
+++ src/pages/execute/donutChart.jsx
@@ -3,14 +3,106 @@ import React, { useState, useEffect,useRef } from "react";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// FIND A DATA DID FLOWED IN. FIND A WAY TO UPDATE THE CHART. 
+
+const arc = d3.arc()
+        .innerRadius(100)
+        .outerRadius(150)
+
+const color = d3.scaleOrdinal()
+.range(['#8BC34A', '#F44336', '#9C27B0', '#E91E63', '#673AB7', '#3F51B5', '#2196F3', '#00BCD4', '#4CAF50', '#CDDC39']);
+
+const changeChartPositionOfValues = (dimensions) => {
+    // Determine values
+    const variablesToWorkWith = {
+      completionLevel: null,
+      leftToComplete: null
+    }
+    
+    variablesToWorkWith.completionLevel = dimensions[0].value;
+    variablesToWorkWith.leftToComplete = dimensions[1].value;
+
+    // Determine the degree for each of these variables
+    const denominator = variablesToWorkWith.completionLevel + variablesToWorkWith.leftToComplete;
+    const degrees = {
+      completionLevel: null,
+      leftToComplete: null
+    }
+    for(const key in variablesToWorkWith){
+        const degree = variablesToWorkWith[key]/denominator * 360
+        degrees[key] = degree
+    }
+    
+    // Determine radian
+    const radians = {
+        completionLevel: null,
+        leftToComplete: null
+    }
+    const pi = Math.PI;
+    for(const key in degrees){
+        const degree = degrees[key]
+        const radian = degree * (pi/180);
+        radians[key] = radian
+    }
+
+    // Determine startAngle and endAngle for both parties. 
+    const startAndEndAngles = {
+        completionLevel: {
+            startAngle: null,
+            endAngle: null
+        },
+        leftToComplete: {
+            startAngle: null,
+            endAngle: null
+        }
+    }
+    for(const key in radians){
+        
+        if(key === "completionLevel"){
+            startAndEndAngles[key].startAngle = 0
+            startAndEndAngles[key].endAngle = radians[key]
+        }
+
+        if(key === "leftToComplete"){
+            const startingAngle = startAndEndAngles["completionLevel"].endAngle
+            startAndEndAngles[key].startAngle = startingAngle
+            startAndEndAngles[key].endAngle = startingAngle + radians[key]
+        }
+    }
+    
+    // Change startAndEndAngles found in dimensions
+    dimensions[0].startAngle = startAndEndAngles.completionLevel.startAngle;
+    dimensions[0].endAngle = startAndEndAngles.completionLevel.endAngle;
+    dimensions[1].startAngle = startAndEndAngles.leftToComplete.startAngle;
+    dimensions[1].endAngle = startAndEndAngles.leftToComplete.endAngle;
+
+    // console.log("values: ",variablesToWorkWith)
+    // console.log("Degrees: ",degrees)
+    // console.log("Radians: ",radians)
+    // console.log("startAndEndAngles: ",startAndEndAngles)
+    // console.log("EDITED original dimensions: ",dimensions);
+    // console.log(radians.completionLevel+radians.leftToComplete)
+
+    return dimensions
+}
 
 export default function DonutChart(prop){
         console.log(prop.current)
-        const chartContainer = useRef(null)
+        // My goal is to attach the donut chart to the execute script. 
+        // Everytime a new trade is made, the back-end will send data (counter, trade data) to the front-end's socket.
+        // The front-end will run a function to store the data into two useStates (counter, tradeData)
+        // DonutChart looks at the counter useState. 
+        
+        console.log("Donut: ",prop.current)
+        const [chartData,setChartData] = useState([0,10]);
+        const [handleChange, setHandleChange] = useState(null);
+        const chartContainer = useRef(null);
 
         const createChart = () => {
-            const data = [prop.max,prop.current]
+            //const data = [chartData.max - chartData.current,chartData.current];
             //Create a place to display the chart: SVG element
+
+            // Get the container
             const svg = d3.select(chartContainer.current)
                 .append('svg')
                 .attr('width',300)
@@ -18,48 +110,100 @@ export default function DonutChart(prop){
             
             // Create the chart
                 // Create the chart itself
-                const pieChart = d3.pie()(data)
 
-                // Create the settings
-                    // Create the arc
-                    const arc = d3.arc()
-                        .innerRadius(100)
-                        .outerRadius(150)
-                
-                    // Establish colors
-                    const color = d3.scaleOrdinal()
-                        .range(['#9C27B0', '#F44336', '#E91E63', '#673AB7', '#3F51B5', '#2196F3', '#00BCD4', '#4CAF50', '#8BC34A', '#CDDC39']);
+                // let pieChart = d3.pie()(chartData);
+                // console.log("WHat is going on????: ",pieChart)
+                // const reOrder = changeChartPositionOfValues(pieChart)
+                // pieChart = changeChartPositionOfValues(pieChart)
+                // console.log("lalal: ",reOrder)
 
             // Putting the container, the chart, and the settings together
                 // Put the chart into the container
-                const g = svg.append('g')
-                    .attr('transform', `translate(${svg.attr('width') / 2},${svg.attr('height') / 2})`)
+                const g = svg.append('g')          
+                    .attr('transform', `translate(${svg.attr('width') / 2},${svg.attr('height') / 2})`)     
 
                 // Put the settings into the container
-                const slices = g.selectAll('path')
-                    .data(pieChart)
-                    .enter()
-                    .append('path')
-                    .attr('d', arc)
-                    .attr('fill', (d,i) => color(i)) 
-        }
 
+                // const slices = g.selectAll('path')
+                //     .data(pieChart)
+                //     .enter()
+                //     .append('path')
+                //     .attr('d', arc)
+                //     .attr('fill', (d,i) => color(i)) 
+
+                setHandleChange(g)
+        }
+        
+        const updateProgress = () => {
+            
+            if(handleChange !== null){
+                console.log("CHECKING DATA EVERYTIME CHART UPDATES: ",chartData)
+                const dimensions = d3.pie()(chartData);
+                const reorderDimensions = changeChartPositionOfValues(dimensions);
+                console.log(reorderDimensions)
+                const path = handleChange.selectAll('paths').data(reorderDimensions)
+                path.enter().append("path")
+                .attr('d',arc)
+                .attr('fill',(d,i)=>color(i));
+            }
+
+        }
+        
         const preventMultipleChartsFromAppearing = () => {
             if(chartContainer.current.children.length === 0){
-                createChart()
+                createChart();
+                updateProgress()
             }
         }
 
         useEffect(()=>{
             preventMultipleChartsFromAppearing()
-            
         },[])
-    
+
+        useEffect(()=>{
+            updateProgress()
+        },[handleChange])
+
+        useEffect(()=>{
+            if(prop.current!==0){
+                    setChartData([prop.current,10-prop.current])
+            }
+        },[prop.current])
+
+        useEffect(()=>{
+            updateProgress()
+            
+            console.log("THIS THIS THIS: ",chartData)
+        },[chartData])
+        // useEffect(()=>{
+        //     if(handleChange!==null){
+        //         updateProgress()
+        //     }
+        // },[handleChange]) 
+
+        // useEffect(()=>{
+            
+        //     updateProgress()
+        // },[chartData])
+        
+        // useEffect(()=>{
+            
+        //     const previous = chartData[0];
+        //     console.log("CHECKING THE NUMBERS: ",prop.current, 10-prop.current)
+        //     setChartData([prop.current,10-prop.current])
+            
+        // },[prop.current])
+
+        // useEffect(()=>{
+        //     console.log(chartData)
+        //     // preventMultipleChartsFromAppearing()
+        // },[chartData])
+
         return(
             <div className='m-4'>
                 <h5>{prop.title}</h5>
-                <div ref={chartContainer}></div>
-                
+                <div ref={chartContainer}></div> 
             </div>
         )
 }
+
